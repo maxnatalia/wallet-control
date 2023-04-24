a28e2be55c0967252543c20d7be30a2f64a9b08b
maxnatalia <Nataliamazur1988@gmail.com>
2023-04-24 13:02:13 +0200

    added data for app: categories, radio

---
+++ src/utils/data.js
@@ -0,0 +1,5 @@
+export const categoriesData = ["Jedzenie", "Mieszkanie", "Transport", "Rozrywka", "Zdrowie", "Edukacja", "Wynagrodzenie", "Sprzedaż", "Odsetki"];
+export const radioData = [
+    { label: "Income", value: "income" },
+    { label: "Expense", value: "expense" }
+];
