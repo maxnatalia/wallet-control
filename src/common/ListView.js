c79d3ed8ccf81da450797d4ff1fe0ca45539bb1c
maxnatalia <Nataliamazur1988@gmail.com>
2023-04-29 11:54:27 +0200

    updated ListView - added avatars instead bgColor

--- src/common/ListView.js
+++ src/common/ListView.js
@@ -1,9 +1,9 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Avatar } from "@mui/material";
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
-import { lightGreen, red } from "@mui/material/colors";
+import { green, red } from "@mui/material/colors";
 import { formatPrice } from "../utils/formatPrice";
 
-const ListView = ({ listItems, removeItem }) => {
+const ListView = ({ listItems, removeItem, currency }) => {
 
     return (
         <TableContainer component={Paper} sx={{ mb: 3 }}>
@@ -22,12 +22,7 @@ const ListView = ({ listItems, removeItem }) => {
                     )}
                 <TableBody>
                     {listItems.map((row) => (
-                        <TableRow
-                            sx={{
-                                backgroundColor: row.variant === "income" ? lightGreen[100] : red[100],
-                            }}
-                            key={row.id}
-                        >
+                        <TableRow key={row.id}>
                             <TableCell component="th" scope="row">
                                 {row.category}
                             </TableCell>
@@ -38,10 +33,20 @@ const ListView = ({ listItems, removeItem }) => {
                                     </IconButton>
                                 </Tooltip>
                             </TableCell>
-                            <TableCell align="left">{(row.variant.toUpperCase())}</TableCell>
+                            <TableCell align="left">
+                                {row.variant === "expense"
+                                    ? <Avatar sx={{ bgcolor: red[500] }}>
+                                        E
+                                    </Avatar>
+                                    :
+                                    <Avatar sx={{ bgcolor: green[500] }}>
+                                        I
+                                    </Avatar>
+                                }
+                            </TableCell>
                             <TableCell align="left">{row.description}</TableCell>
                             <TableCell align="left">{row.date}</TableCell>
-                            <TableCell align="left">{formatPrice(row.amount, row.currency)}</TableCell>
+                            <TableCell align="left">{formatPrice(row.amount, currency)}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
