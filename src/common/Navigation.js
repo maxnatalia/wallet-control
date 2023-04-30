6b8849bb9bfde30d156617bc70c17b48fcc95cfd
maxnatalia <Nataliamazur1988@gmail.com>
2023-04-30 18:56:58 +0200

    added new IconButton - change currency in nav

--- src/common/Navigation.js
+++ src/common/Navigation.js
@@ -1,37 +1,87 @@
-import { AppBar, IconButton, Switch, Toolbar, Typography } from '@mui/material'
+import { useState } from 'react';
+import { AppBar, IconButton, Switch, Toolbar, Typography, Tooltip, Menu, MenuItem, Fade } from '@mui/material'
 import WalletIcon from '@mui/icons-material/Wallet';
+import PaidIcon from '@mui/icons-material/Paid';
 import { useTodayDate } from '../utils/useTodayDate';
+import { currencyData } from '../utils/data';
 
-const Navigation = () => {
+const Navigation = ({ handleModeChange, darkMode, currency, handleCurrency }) => {
     const todayDate = useTodayDate();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+        setOpen(false);
+    };
 
     return (
-        <>
-            <AppBar position="sticky" >
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="wallet-icon"
-                        sx={{ mr: 2 }}
-                    >
-                        <WalletIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ display: { xs: 'none', md: 'block' } }}>
-                        Wallet Control
-                    </Typography>
+        <AppBar position="sticky" >
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="wallet-icon"
+                    sx={{ mr: 2 }}
+                >
+                    <WalletIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ display: { xs: 'none', md: 'block' } }}>
+                    Wallet Control
+                </Typography>
+                <Tooltip title="Change the currency" placement="right-start">
+                    <div>
+                        <IconButton
+                            id="fade-button"
+                            aria-controls={open ? 'fade-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}
+                        >
+                            <PaidIcon sx={{ color: "white" }} />
+                        </IconButton>
+                        <Typography variant='h6' component="span">
+                            {currency}
+                        </Typography>
+                        <Menu
+                            id="fade-menu"
+                            MenuListProps={{
+                                'aria-labelledby': 'fade-button',
+                            }}
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            TransitionComponent={Fade}
+                        >
+                            {currencyData.map((item) => (
+                                <MenuItem key={item} onClick={() => { handleClose(); handleCurrency(item); }}>
+                                    {item}
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </div>
+                </Tooltip>
+                <Tooltip title="Today's date">
                     <Typography variant="h6" component="span" sx={{ flexGrow: 1 }} textAlign="center" color="inherit">
-                        Balance up to date - {todayDate}
+                        {todayDate}
                     </Typography>
+                </Tooltip>
+                <Tooltip title={darkMode === "light" ? "Switch to dark mode" : "Switch to light mode"}>
                     <Switch
                         color="default"
-                        defaultChecked
-                        aria-label="login switch"
+                        checked={darkMode}
+                        aria-label="theme switch"
+                        onChange={handleModeChange}
                     />
-                </Toolbar>
-            </AppBar>
-        </>
+                </Tooltip>
+            </Toolbar>
+        </AppBar >
     )
 };
 
