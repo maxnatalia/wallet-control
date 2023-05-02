7572daef165a655e601f1b93b6ba9d14da1bd9b0
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-02 13:27:02 +0200

    improved currency display

--- src/common/Result.js
+++ src/common/Result.js
@@ -3,9 +3,8 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import { blue, lightGreen, red } from '@mui/material/colors';
 import { formatPrice } from '../utils/formatPrice';
 
-const Result = ({ fields, totalAmount }) => {
+const Result = ({ totalAmount, currency }) => {
 
-    const { currency } = fields;
     return (
         <Tooltip title="Wallet Balance">
             <Box sx={{ backgroundColor: blue[100], p: 1, mt: 2, ml: 2, borderRadius: 2, display: "flex", alignItems: "center", position: "fixed", zIndex: 2 }}>
