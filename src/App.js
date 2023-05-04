ecb3daab03fd2a4913e3d57191f22461a715c4e0
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-04 07:24:58 +0200

    updated main App component, improved paths

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import { Typography, Box } from "@mui/material";
-import InputField from "./common/InputField";
-import SelectField from "./common/SelectField";
-import Form from "./common/Form";
+import { Container, Box, ThemeProvider } from "@mui/material";
+import ViewItems from "./common/ViewItems";
+import Navigation from "./common/Navigation";
+import FormBudget from "./common/FormBudget";
+import Result from "./common/Result";
+import { useFormBudget } from "./common/useFormBudget";
+import useThemeMode from "./utils/useThemeMode";
 
 const App = () => {
-  return (
-    <Box>
-      <Typography variant="h1">
-        Wallet Control
-      </Typography>
-      <Box>
-        <Typography variant="h3"> Form </Typography>
-        <Form>
-          <InputField
-            helperText="Specify the expense/income name"
-            label="Text label"
-            type="text"
-            name="item"
-            placeholder="Text label"
-          />
-          <SelectField
-            helperText="Choose category"
-            label="Category"
-            name="select"
-            options={["raz", "dwa"]}
-          />
-        </Form>
-      </Box>
-      <Typography variant="h3">
+  const { listItems, removeItem, fields, handleFieldChange, handleSubmit, validationAmount, totalAmount, currency, handleCurrency } = useFormBudget();
+  const { darkMode, handleModeChange, theme } = useThemeMode();
 
-        List
-      </Typography>
-    </Box>
+  return (
+    <ThemeProvider theme={theme}>
+      <Box bgcolor={"background.default"} >
+        <Navigation
+          darkMode={darkMode}
+          handleModeChange={handleModeChange}
+          currency={currency}
+          handleCurrency={handleCurrency}
+        />
+        <Result
+          totalAmount={totalAmount}
+          currency={currency} />
+        <Container
+          maxWidth="md"
+          sx={{
+            backgroundColor: "background.default",
+            color: "text.primary",
+            borderRadius: 2,
+            p: 2,
+            mt: 2
+          }}
+        >
+          <FormBudget
+            listItems={listItems}
+            removeItem={removeItem}
+            fields={fields}
+            handleFieldChange={handleFieldChange}
+            handleSubmit={handleSubmit}
+            validationAmount={validationAmount}
+          />
+          <ViewItems
+            listItems={listItems}
+            removeItem={removeItem}
+            currency={currency}
+          />
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
