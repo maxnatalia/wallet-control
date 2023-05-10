641c7e21bb05863a6fa0f87f336c2152aee147cd
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-10 09:22:06 +0200

    created new component Result

--- src/common/Result/index.js
+++ src/common/Result/index.js
@@ -1,26 +1,49 @@
-import { Box, Tooltip, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Button, Tooltip, Typography } from '@mui/material';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import { blue, lightGreen, red } from '@mui/material/colors';
 import { formatPrice } from '../../utils/formatPrice';
 
 const Result = ({ totalAmount, currency }) => {
+    const [showResult, setShowResult] = useState(false);
+
+    const handleShowResult = () => {
+        setShowResult(!showResult);
+    };
 
     return (
-        <Tooltip title="Wallet Balance">
-            <Box sx={{ backgroundColor: blue[100], p: 1, mt: 2, ml: 2, borderRadius: 2, display: "flex", alignItems: "center", position: "fixed", zIndex: 2 }}>
-                <AccountBalanceWalletIcon
-                    sx={{ p: 1, color: "primary.main", fontSize: { xs: 20, md: 30 } }} />
-                <Typography
-                    variant='h6'
-                    component="span"
-                    sx={{
-                        fontWeight: 900,
-                        fontSize: 20,
-                        color: totalAmount > 0 ? lightGreen[600] : red[400],
-                    }}>
-                    {formatPrice(totalAmount, currency)}
-                </Typography>
-            </Box>
+        <Tooltip title={!showResult ? "Show Wallet Balance" : "Hide Wallet Balance"}>
+            <Button
+                onClick={handleShowResult}
+                startIcon={<AccountBalanceWalletIcon />}
+                sx={{
+                    backgroundColor: blue[100],
+                    p: 1,
+                    mt: 1,
+                    ml: 1,
+                    borderRadius: 2,
+                    position: "fixed",
+                    zIndex: 2,
+                    '&:hover': {
+                        backgroundColor: blue[200],
+                    },
+                    '&:active': {
+                        backgroundColor: blue[200],
+                    },
+                }}
+            >
+                {showResult &&
+                    <Typography
+                        variant='h6'
+                        component="span"
+                        sx={{
+                            fontWeight: 900,
+                            color: totalAmount > 0 ? lightGreen[600] : red[400],
+                        }}>
+                        {formatPrice(totalAmount, currency)}
+                    </Typography>
+                }
+            </Button>
         </Tooltip>
     )
 };
