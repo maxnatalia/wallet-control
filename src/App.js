33ba1606a95631b123d33a1712ca4ac639687a00
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-15 11:03:12 +0200

    updated paths for imports and refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import { Container, Box, ThemeProvider } from "@mui/material";
-import ViewItems from "./common/ViewItems";
-import Navigation from "./common/Navigation";
-import FormBudget from "./common/FormBudget";
-import Result from "./common/Result";
-import { useFormBudget } from "./common/useFormBudget";
-import useThemeMode from "./utils/useThemeMode";
+import ViewItems from "./ViewItems";
+import Navigation from "./Navigation";
+import FormBudget from "./FormBudget";
+import Result from "./Result";
+import { useFormBudget } from "./FormBudget/useFormBudget";
+import { useThemeMode } from "./Navigation/ThemeSwitcher/useThemeMode";
+import { useAddCategory } from "./Navigation/ManagementCategories/CategorySetup/useAddCategory";
 
 const App = () => {
-  const { listItems, removeItem, fields, handleFieldChange, handleSubmit, validationAmount, totalAmount, currency, handleCurrency } = useFormBudget();
+  const {
+    listItems,
+    removeItem,
+    fields,
+    handleFieldChange,
+    handleSubmit,
+    validationAmount,
+    totalAmount,
+    currency,
+    handleCurrency,
+    editItem,
+    editableId,
+    handleClearAll,
+    filteredData,
+    handleResetFilters
+  } = useFormBudget();
+
   const { darkMode, handleModeChange, theme } = useThemeMode();
+  const { handleAddCategory, categories, setCategories } = useAddCategory();
 
   return (
     <ThemeProvider theme={theme}>
@@ -18,10 +36,15 @@ const App = () => {
           handleModeChange={handleModeChange}
           currency={currency}
           handleCurrency={handleCurrency}
+          handleAddCategory={handleAddCategory}
+          categories={categories}
+          setCategories={setCategories}
+          listItems={listItems}
         />
         <Result
           totalAmount={totalAmount}
-          currency={currency} />
+          currency={currency}
+        />
         <Container
           maxWidth="md"
           sx={{
@@ -35,15 +58,27 @@ const App = () => {
           <FormBudget
             listItems={listItems}
             removeItem={removeItem}
+            editItem={editItem}
             fields={fields}
+            editableId={editableId}
             handleFieldChange={handleFieldChange}
             handleSubmit={handleSubmit}
             validationAmount={validationAmount}
+            categories={categories}
+            darkMode={darkMode}
           />
           <ViewItems
             listItems={listItems}
             removeItem={removeItem}
+            editItem={editItem}
             currency={currency}
+            handleClearAll={handleClearAll}
+            darkMode={darkMode}
+            categories={categories}
+            filteredData={filteredData}
+            handleFieldChange={handleFieldChange}
+            fields={fields}
+            handleResetFilters={handleResetFilters}
           />
         </Container>
       </Box>
