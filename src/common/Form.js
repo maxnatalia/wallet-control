b6951e65d769f96d336a692ab8faaf2a76b19fcf
maxnatalia <Nataliamazur1988@gmail.com>
2023-05-15 11:02:49 +0200

    refactoring Form component

--- src/common/Form.js
+++ src/common/Form.js
@@ -3,7 +3,9 @@ import { Box } from '@mui/material'
 const Form = ({ children, onSubmit }) => {
     return (
         <Box variant="outlined">
-            <form onSubmit={onSubmit}>{children}</form>
+            <form onSubmit={onSubmit}>
+                {children}
+            </form>
         </Box>
     )
 };
